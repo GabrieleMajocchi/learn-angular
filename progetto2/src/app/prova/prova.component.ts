5e41ab5cc64861a861b1788d53416d93980df1e2
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-12-15 19:03:02 +0100

    added OnInit

--- progetto2/src/app/prova/prova.component.ts
+++ progetto2/src/app/prova/prova.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-prova',
   templateUrl: './prova.component.html',
   styleUrl: './prova.component.css'
 })
-export class ProvaComponent {
+export class ProvaComponent implements OnInit{
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 
 }
