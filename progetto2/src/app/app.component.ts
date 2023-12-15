0d6b9a40b934563f8395d5b1f3a6d43123e3722d
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-12-15 18:58:00 +0100

    added comment on the app.component.ts

--- progetto2/src/app/app.component.ts
+++ progetto2/src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // --Nome del tag dove importeremo questo componente--
   selector: 'app-root',
+  // --Il template del componente--
   templateUrl: './app.component.html',
+  // --Lo stile del componente--
   styleUrl: './app.component.css'
 })
 export class AppComponent {
