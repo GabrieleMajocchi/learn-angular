06f342b58eb0638f2d462c87349952f6980c31a0
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-12-18 20:42:33 +0100

    added Angular material and slide toggle module to the prova component

--- progetto2/src/app/app.module.ts
+++ progetto2/src/app/app.module.ts
@@ -4,7 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProvaComponent } from './prova/prova.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +13,9 @@ import { ProvaComponent } from './prova/prova.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
