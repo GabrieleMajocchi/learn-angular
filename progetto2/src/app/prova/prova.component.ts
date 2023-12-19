aef879097dcbe2f785e202a0d1a3c1941d8ab96c
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-12-19 11:47:09 +0100

    added string interpolation

--- progetto2/src/app/prova/prova.component.ts
+++ progetto2/src/app/prova/prova.component.ts
@@ -7,6 +7,14 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
 })
 export class ProvaComponent implements OnInit, AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, DoCheck, OnDestroy{
 
+  cani = [
+    {nome: 'roger',
+    razza: 'golden retriver',
+    descrizione:`The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+  originally bred for hunting.`}
+  ]
+
   constructor() {
     console.log("costruttore")
   }
